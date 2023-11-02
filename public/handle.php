ab9047e3cee840cf5c2b0819c76a1d831eab67d8
TrongNhan030602 <ntnhan030602@gmail.com>
2023-11-02 16:33:06 +0700

    "Một số file ban đầu"

---
+++ public/handle.php
@@ -0,0 +1,14 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["table"]) && !empty($_POST["action"])) {
+  $selectedtable = $_POST["table"];
+  $selectedaction = $_POST["action"];
+  echo "Bạn đã chọn bảng: ". "<strong>$selectedtable</strong>" ." và chọn thao tác: ". "<strong>$selectedaction</strong>";
+}
+else {
+    header('location: index.php');
+    echo 'Bạn phải chọn cả bảng và thao tác';
+}
+
+
+
+?>
